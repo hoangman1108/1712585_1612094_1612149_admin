1f3327d18b55a70d4f15a4c9f1fbb13def7cdd2b
TuongDuyTran <duyithcmus2016@gmail.com>
2022-01-10 00:14:31 +0700

    add init ManualMapModel.js

---
+++ src/components/_dashboard/user/components/ManualMapModel.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useDashboard from '../../../../hooks/useDashboard';
+
+export default function ManualMapModel({ open, setOpen, email }) {
+  const { deleteAdmin } = useDashboard();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Xóa Admin Account</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Bạn có chắc muốn xóa admin với email: <span style={{ color: 'red' }}>{email}</span>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} style={{ color: '#4a4a4a' }}>
+            Disagree
+          </Button>
+          <Button
+            onClick={() => {
+              deleteAdmin(email);
+            }}
+            autoFocus
+          >
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
