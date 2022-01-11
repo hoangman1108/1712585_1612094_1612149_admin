936c7731ad7c82e0bd91cebf5812f83381e3f4a4
DatNC <datnc@younetgroup.com>
2022-01-11 22:20:36 +0700

    update detail model

--- src/components/_dashboard/user/components/DetailModel.js
+++ src/components/_dashboard/user/components/DetailModel.js
@@ -1,15 +1,18 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
-export default function DetailModel({ open, setOpen }) {
+export default function DetailModel({ open, setOpen, info }) {
   const handleClose = () => {
     setOpen(false);
   };
+  console.log("info ne", info);
+
+  const { id, name, dob, mssv, role, phone, email, status } = info;
 
   return (
     <div>
@@ -19,24 +22,43 @@ export default function DetailModel({ open, setOpen }) {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">Xóa Admin Account</DialogTitle>
+        <DialogTitle id="alert-dialog-title">Detail user</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Bạn có chắc muốn xóa admin với email
+            <div>
+              <h6></h6>
+              <span>{`Name: ${name}`}</span>
+            </div>
+            <div>
+              <span>{`Birthday: ${dob}`}</span>
+            </div>
+            <div>{role === "admin" ? "" : <span>{`Mssv: ${mssv}`}</span>}</div>
+            <div>
+              <span>{`Role: ${role}`}</span>
+            </div>
+            <div>
+              <span>{`Phone: ${phone ? phone : "not data"}`}</span>
+            </div>
+            <div>
+              <span>{`Email: ${email}`}</span>
+            </div>
+            <div>
+              <span>{`Status: ${status}`}</span>
+            </div>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} style={{ color: '#4a4a4a' }}>
-            Disagree
+          <Button onClick={handleClose} style={{ color: "#4a4a4a" }}>
+            Close
           </Button>
-          <Button
+          {/* <Button
             // onClick={() => {
             //   deleteAdmin(email);
             // }}
             autoFocus
           >
             Agree
-          </Button>
+          </Button> */}
         </DialogActions>
       </Dialog>
     </div>
