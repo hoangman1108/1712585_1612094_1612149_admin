f0fe166ae8cf9ec6ca3b7d89953fd817a291adc3
DatNC <datnc@younetgroup.com>
2022-01-11 22:20:07 +0700

    add detail for class

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,9 +1,9 @@
-import { filter } from 'lodash';
-import { Icon } from '@iconify/react';
-import { sentenceCase } from 'change-case';
-import { useState } from 'react';
-import plusFill from '@iconify/icons-eva/plus-fill';
-import { Link as RouterLink } from 'react-router-dom';
+import { filter } from "lodash";
+import { Icon } from "@iconify/react";
+import { sentenceCase } from "change-case";
+import { useState, useRef } from "react";
+import plusFill from "@iconify/icons-eva/plus-fill";
+import { Link as RouterLink } from "react-router-dom";
 import {
   Card,
   Table,
@@ -17,23 +17,30 @@ import {
   Container,
   Typography,
   TableContainer,
-  TablePagination
-} from '@mui/material';
-import CreateAccountModal from '../components/modals/CreateAccountAdminModal';
-import Page from '../components/Page';
-import Label from '../components/Label';
-import Scrollbar from '../components/Scrollbar';
-import SearchNotFound from '../components/SearchNotFound';
-import { UserListHead, UserListToolbar, UserMoreMenu } from '../components/_dashboard/user';
+  TablePagination,
+  IconButton,
+} from "@mui/material";
+import moreVerticalFill from "@iconify/icons-eva/more-vertical-fill";
+import CreateAccountModal from "../components/modals/CreateAccountAdminModal";
+import Page from "../components/Page";
+import Label from "../components/Label";
+import Scrollbar from "../components/Scrollbar";
+import SearchNotFound from "../components/SearchNotFound";
+import {
+  UserListHead,
+  UserListToolbar,
+  UserMoreMenu,
+} from "../components/_dashboard/user";
 
-import useDashboard from '../hooks/useDashboard';
+import useDashboard from "../hooks/useDashboard";
+import ClassModel from "../components/_dashboard/user/ClassModel";
 
 const TABLE_HEAD = [
-  { id: 'name', label: 'Tên lớp', alignRight: false },
-  { id: 'codeJoin', label: 'Code Join', alignRight: false },
-  { id: 'numTeacher', label: 'Teachers', alignRight: false },
-  { id: 'numStudent', label: 'Students', alignRight: false },
-  { id: 'numAssignment', label: 'Assignments', alignRight: false }
+  { id: "name", label: "Tên lớp", alignRight: false },
+  { id: "codeJoin", label: "Code Join", alignRight: false },
+  { id: "numTeacher", label: "Teachers", alignRight: false },
+  { id: "numStudent", label: "Students", alignRight: false },
+  { id: "numAssignment", label: "Assignments", alignRight: false },
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -47,7 +54,7 @@ function descendingComparator(a, b, orderBy) {
 }
 
 function getComparator(order, orderBy) {
-  return order === 'desc'
+  return order === "desc"
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
@@ -60,25 +67,32 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    return filter(
+      array,
+      (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
   }
   return stabilizedThis.map((el) => el[0]);
 }
 
 export default function Classes() {
   const [page, setPage] = useState(0);
-  const [order, setOrder] = useState('asc');
+  const [order, setOrder] = useState("asc");
   const [selected, setSelected] = useState([]);
-  const [orderBy, setOrderBy] = useState('name');
-  const [filterName, setFilterName] = useState('');
+  const [orderBy, setOrderBy] = useState("name");
+  const [filterName, setFilterName] = useState("");
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [openCreate, setOpenCreate] = useState(false);
+  const ref = useRef(null);
   const { classes } = useDashboard();
-  console.log('classes: ', classes);
+  console.log("classes: ", classes);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [info, setInfo] = useState({});
 
   const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
 
@@ -113,16 +127,26 @@ export default function Classes() {
     setFilterName(event.target.value);
   };
 
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - classes.length) : 0;
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - classes.length) : 0;
 
-  const filteredUsers = applySortFilter(classes, getComparator(order, orderBy), filterName);
+  const filteredUsers = applySortFilter(
+    classes,
+    getComparator(order, orderBy),
+    filterName
+  );
 
   const isUserNotFound = filteredUsers.length === 0;
 
   return (
     <Page title="User | Minimal-UI">
       <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
           <Typography variant="h4" gutterBottom>
             Danh sách Lớp học
           </Typography>
@@ -161,7 +185,14 @@ export default function Classes() {
                   {filteredUsers
                     .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     .map((row) => {
-                      const { id, name, codeJoin, teachers, students, assignments } = row;
+                      const {
+                        id,
+                        name,
+                        codeJoin,
+                        teachers,
+                        students,
+                        assignments,
+                      } = row;
                       const isItemSelected = selected.indexOf(name) !== -1;
 
                       return (
@@ -180,7 +211,11 @@ export default function Classes() {
                             /> */}
                           </TableCell>
                           <TableCell component="th" scope="row" padding="left">
-                            <Stack direction="row" alignItems="left" spacing={2}>
+                            <Stack
+                              direction="row"
+                              alignItems="left"
+                              spacing={2}
+                            >
                               {/* <Avatar alt={name} src={avatarUrl} /> */}
                               <Typography variant="subtitle2" noWrap>
                                 {name}
@@ -188,13 +223,38 @@ export default function Classes() {
                             </Stack>
                           </TableCell>
                           <TableCell align="left">{codeJoin}</TableCell>
-                          <TableCell align="left">{teachers?.length || 0}</TableCell>
-                          <TableCell align="left">{students?.length || 0}</TableCell>
-                          <TableCell align="left">{assignments?.length || 0}</TableCell>
-                          {/* 
+                          <TableCell align="left">
+                            {teachers?.length || 0}
+                          </TableCell>
+                          <TableCell align="left">
+                            {students?.length || 0}
+                          </TableCell>
+                          <TableCell align="left">
+                            {assignments?.length || 0}
+                          </TableCell>
+
                           <TableCell align="right">
-                            <UserMoreMenu />
-                          </TableCell> */}
+                            <IconButton
+                              ref={ref}
+                              onClick={() => {
+                                setIsOpen(true);
+                                setInfo({
+                                  id,
+                                  name,
+                                  codeJoin,
+                                  teachers,
+                                  students,
+                                  assignments,
+                                });
+                              }}
+                            >
+                              <Icon
+                                icon={moreVerticalFill}
+                                width={20}
+                                height={20}
+                              />
+                            </IconButton>
+                          </TableCell>
                         </TableRow>
                       );
                     })}
@@ -227,6 +287,7 @@ export default function Classes() {
             onRowsPerPageChange={handleChangeRowsPerPage}
           />
         </Card>
+        <ClassModel open={isOpen} setOpen={setIsOpen} info={info} />
       </Container>
       <CreateAccountModal open={openCreate} setOpen={setOpenCreate} />
     </Page>
