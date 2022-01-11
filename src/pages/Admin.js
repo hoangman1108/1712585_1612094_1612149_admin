1beb2934512f2338f6a70ff04400c650de1e81d1
DatNC <datnc@younetgroup.com>
2022-01-11 22:20:16 +0700

    add detail for admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,9 +1,9 @@
-import { filter } from 'lodash';
-import { Icon } from '@iconify/react';
-import { sentenceCase } from 'change-case';
-import { useState } from 'react';
-import plusFill from '@iconify/icons-eva/plus-fill';
-import { Link as RouterLink } from 'react-router-dom';
+import { filter } from "lodash";
+import { Icon } from "@iconify/react";
+import { sentenceCase } from "change-case";
+import { useState } from "react";
+import plusFill from "@iconify/icons-eva/plus-fill";
+import { Link as RouterLink } from "react-router-dom";
 import {
   Card,
   Table,
@@ -17,24 +17,28 @@ import {
   Container,
   Typography,
   TableContainer,
-  TablePagination
-} from '@mui/material';
-import CreateAccountModal from '../components/modals/CreateAccountAdminModal';
-import Page from '../components/Page';
-import Label from '../components/Label';
-import Scrollbar from '../components/Scrollbar';
-import SearchNotFound from '../components/SearchNotFound';
-import { UserListHead, UserListToolbar, UserMoreMenu } from '../components/_dashboard/user';
+  TablePagination,
+} from "@mui/material";
+import CreateAccountModal from "../components/modals/CreateAccountAdminModal";
+import Page from "../components/Page";
+import Label from "../components/Label";
+import Scrollbar from "../components/Scrollbar";
+import SearchNotFound from "../components/SearchNotFound";
+import {
+  UserListHead,
+  UserListToolbar,
+  UserMoreMenu,
+} from "../components/_dashboard/user";
 
-import useDashboard from '../hooks/useDashboard';
+import useDashboard from "../hooks/useDashboard";
 
 const TABLE_HEAD = [
-  { id: 'name', label: 'Name', alignRight: false },
-  { id: 'email', label: 'Email', alignRight: false },
-  { id: 'role', label: 'Role', alignRight: false },
-  { id: 'dob', label: 'DOB', alignRight: false },
-  { id: 'status', label: 'Status', alignRight: false },
-  { id: '' }
+  { id: "name", label: "Name", alignRight: false },
+  { id: "email", label: "Email", alignRight: false },
+  { id: "role", label: "Role", alignRight: false },
+  { id: "dob", label: "DOB", alignRight: false },
+  { id: "status", label: "Status", alignRight: false },
+  { id: "" },
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -48,7 +52,7 @@ function descendingComparator(a, b, orderBy) {
 }
 
 function getComparator(order, orderBy) {
-  return order === 'desc'
+  return order === "desc"
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
@@ -61,24 +65,27 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    return filter(
+      array,
+      (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
   }
   return stabilizedThis.map((el) => el[0]);
 }
 
 export default function Admin() {
   const [page, setPage] = useState(0);
-  const [order, setOrder] = useState('asc');
+  const [order, setOrder] = useState("asc");
   const [selected, setSelected] = useState([]);
-  const [orderBy, setOrderBy] = useState('name');
-  const [filterName, setFilterName] = useState('');
+  const [orderBy, setOrderBy] = useState("name");
+  const [filterName, setFilterName] = useState("");
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [openCreate, setOpenCreate] = useState(false);
   const { admins } = useDashboard();
 
   const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
 
@@ -122,16 +129,26 @@ export default function Admin() {
     setFilterName(event.target.value);
   };
 
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - admins.length) : 0;
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - admins.length) : 0;
 
-  const filteredUsers = applySortFilter(admins, getComparator(order, orderBy), filterName);
+  const filteredUsers = applySortFilter(
+    admins,
+    getComparator(order, orderBy),
+    filterName
+  );
 
   const isUserNotFound = filteredUsers.length === 0;
 
   return (
     <Page title="User | Minimal-UI">
       <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
           <Typography variant="h4" gutterBottom>
             Danh sách Admin
           </Typography>
@@ -170,7 +187,8 @@ export default function Admin() {
                   {filteredUsers
                     .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     .map((row) => {
-                      const { id, name, role, status, email, avatarUrl, dob } = row;
+                      const { id, name, role, status, email, avatarUrl, dob } =
+                        row;
                       const isItemSelected = selected.indexOf(email) !== -1;
 
                       return (
@@ -189,7 +207,11 @@ export default function Admin() {
                             />
                           </TableCell>
                           <TableCell component="th" scope="row" padding="none">
-                            <Stack direction="row" alignItems="center" spacing={2}>
+                            <Stack
+                              direction="row"
+                              alignItems="center"
+                              spacing={2}
+                            >
                               <Avatar alt={name} src={avatarUrl} />
                               <Typography variant="subtitle2" noWrap>
                                 {name}
@@ -202,14 +224,25 @@ export default function Admin() {
                           <TableCell align="left">
                             <Label
                               variant="ghost"
-                              color={(status === 'banned' && 'error') || 'success'}
+                              color={
+                                (status === "banned" && "error") || "success"
+                              }
                             >
                               {sentenceCase(status)}
                             </Label>
                           </TableCell>
 
                           <TableCell align="right">
-                            <UserMoreMenu email={email} />
+                            <UserMoreMenu
+                              role={role}
+                              email={email}
+                              // mssv={mssv}
+                              id={id}
+                              name={name}
+                              dob={dob}
+                              // phone={phone}
+                              status={status}
+                            />
                           </TableCell>
                         </TableRow>
                       );
